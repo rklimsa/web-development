a67ce5c65d6a7ceb2a3e80fade32d11dada09d8e
Rico <Rico.Klimsa@aol.com>
2016-10-30 00:37:37 +0200

    Some modifications

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var uglify = require('gulp-uglify');
 var clean = require('gulp-clean');
 var childProcess = require('child_process');
 
+var distPath = 'dist/';
+
 gulp.task('init', function () { console.log('Init'); });
 
 gulp.task('compress', function() {
@@ -24,13 +26,17 @@ gulp.task('clean', function () {
 });
 
 gulp.task('build', ['clean','compress'], function() {
-	console.log('build-task');
+	console.log('build completed');
 });
 
 gulp.task('server', function(cb) {
-	childProcess.exec('node src/server.js', function (err, stdout, stderr) {
+	gulp_server_start(cb);
+});
+
+var gulp_server_start = function(cb) {
+	childProcess.exec('node ' + gulp + '/server.js', function (err, stdout, stderr) {
     console.log(stdout);
     console.log(stderr);
     cb(err);
 	})
-});
+};
