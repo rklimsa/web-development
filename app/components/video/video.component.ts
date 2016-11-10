cad948640c819640607d32835c7875bd0e5fb0c6
Rico <Rico.Klimsa@aol.com>
2016-11-10 14:21:02 +0100

    completed video component and added bower

--- app/components/video/video.component.ts
+++ app/components/video/video.component.ts
@@ -1,8 +1,17 @@
     import { Component } from '@angular/core';
+	import { TimeService } from './../../services/timeservice/TimeService';
 
     @Component({
       selector: 'video-component',
-      templateUrl: '/app/components/video/video.html'
+      templateUrl: '/app/components/video/video.html',
+	  providers: [TimeService]
     })
-    export class VideoComponent { 
+    export class VideoComponent {
+		
+		time;
+		
+		constructor(private timeService : TimeService) {
+			this.time = timeService.actualTime();
+		}
+	
 	}
